4ae4e81d1a4d8c58be7d65c3aa82eb213d4580ef
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2024-09-17 20:41:30 -0700

    Test Commit

--- Event Countdown/ContentView.swift
+++ Event Countdown/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("CountDown for Events")
         }
         .padding()
     }
