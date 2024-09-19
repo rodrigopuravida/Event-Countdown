0d0c24bcf63e90a28f761dd9e729e5c47c620fc8
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2024-09-18 17:17:56 -0700

    Adding Event Struct

---
+++ Event Countdown/Event.swift
@@ -0,0 +1,18 @@
+//
+//  Event.swift
+//  Event Countdown
+//
+//  Created by Rodrigo Carballo on 9/18/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct Event {
+    
+    var id: UUID = .init()
+    var title: String
+    var date: Date
+    var textColor: Color
+
+}
