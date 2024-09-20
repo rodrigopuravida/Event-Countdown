a9d84c3a13ce70f13dc2f193b1b6425713c0aff0
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2024-09-19 18:00:18 -0700

    implementing comparable based on date

--- Event Countdown/Event.swift
+++ Event Countdown/Event.swift
@@ -8,11 +8,16 @@
 import Foundation
 import SwiftUI
 
-struct Event {
+struct Event : Identifiable, Comparable{
     
     var id: UUID = .init()
     var title: String
     var date: Date
     var textColor: Color
+    
+    // compares Events based on date
+        static func < (lhs: Event, rhs: Event) -> Bool {
+            return lhs.date < rhs.date
+        }
 
 }
